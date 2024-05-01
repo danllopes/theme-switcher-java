f426dc11ab7e6e7f1efc3a694873728c8a5e03af
“Daniel <danielanderson.lopes0@gmail.com>
2024-05-01 11:10:23 -0300

    Primeiro commit

---
+++ src/main/java/br/com/danllopes/themeswitcher/ThemeSwitcherApplication.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.themeswitcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeSwitcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeSwitcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danllopes/themeswitcher/ThemeSwitcherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.themeswitcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeSwitcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
